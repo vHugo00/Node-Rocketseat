ea2436cd2e464fb97af3a2a7659342a861ce39d8
unknown <vv001226@gmail.com>
2024-09-09 12:20:42 -0300

    feat: initialize middlewares

--- src/server.js
+++ src/server.js
@@ -1,35 +1,30 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
+import { Database } from './middlewares/database.js'
 
-const users = []
-
+const database = new Database
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(users))
+    const users = database.select('users')
+
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
-    users.push({
+
+    const user = {
       id: 1,
       name,
       email,
-    })
+    }
+
+    database.insert('users', user)
+
     return res.writeHead(201).end()
   }
 
