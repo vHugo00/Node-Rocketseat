5284f13826730fe973b31d94276c57b95448daf3
Vitor Hugo <vv001226@gmail.com>
2025-06-10 18:44:47 -0300

    Capturando query parameters

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -3,6 +3,6 @@ export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g
   const pathWithParams = path.replaceAll(routeParametersRegex, '(?<id>[a-z0-9\-_]+)')
 
-  const pathRegex = new RegExp(`^${pathWithParams}`)
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`)
   return pathRegex
 }
